1f361c779c08f665b2e482f6d06c0132a93f2189
REDDERD <daniel.redder@outlook.de>
2024-02-15 09:27:58 +0100

    Fortschritt bei Inlux-Import

--- conversionServices.py
+++ conversionServices.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime as dt
 import pandas as pd
 
 
 def get_datetime_from_unix_timestamp(ts):
-    dt = datetime.fromtimestamp(ts, tz=None)
-    return dt
+    datet = dt.fromtimestamp(ts, tz=None)
+    return datet
 
 
 def is_valid_unix_timestamp(ts):
@@ -17,18 +17,37 @@ def convert_timestamp_iso_to_unix(timestamp):
     timestamp_str = str(timestamp)
     if 'T' in timestamp_str:
         timestamp_str_padded = timestamp_str[:-1] + "0" + timestamp_str[-1] if timestamp_str[-2] == ':' else timestamp_str
-        timestamp_dt = datetime.strptime(timestamp_str_padded, '%Y-%m-%dT%H:%M:%S')
+        timestamp_dt = dt.strptime(timestamp_str_padded, '%Y-%m-%dT%H:%M:%S')
         return timestamp_dt.timestamp()
     else:
         return timestamp
 
-def convert_bytestring_to_unicode_cell(cell_value):
-    decoded_str = cell_value.decode('utf-8')
+def convert_to_datetime(timestamp):
+    # Annahme: Timestamp ist entweder ein String oder ein Unix-Timestamp
+    # Annahme: Bytestrings wurden bereits in Unicode umgewandelt
+    format_iso = "%Y-%m-%dT%H:%M:%S"
     try:
-        new_value = float(decoded_str)
+        val = float(timestamp)
+    except Exception as e:
+        val = timestamp
+
+    if isinstance(val, str):
+        return dt.strptime(val, format_iso)
+    elif isinstance(val, float):
+        return dt.fromtimestamp(val)
+    else:
+        # Bei Fehler wird Jahr 2100 zurückgegeben
+        # Kein Error-Handling, da dies bei der Ausreißer-Erkennung auffallen sollte
+        return dt.strptime("2100-01-01T00:00:00", format_iso)
+
+def convert_bytestring_to_unicode(value):
+    return value.decode('utf-8')
+
+def try_convert_to_float(value):
+    try:
+        return float(value)
     except ValueError:
-        new_value = cell_value.decode('utf-8')
-    return new_value
+        return value
 
 def getYearFromDf(dataframe):
     if not None:
@@ -49,3 +68,25 @@ def getYearFromDf(dataframe):
 
 def dataframe_column_average(df, column_name):
     return df[column_name].mean()
+
+def convert_for_influx(df):
+    columns = df.keys().tolist()
+    # columns.remove("timestamp")
+
+    for index, row in df.iterrows():
+        # Für alle Spalten außer timestamp durchlaufen
+        for column in columns:
+            if isinstance(row[column], bytes):
+                df.at[index, column] = convert_bytestring_to_unicode(row[column])
+            if isinstance(row[column], str):
+                df.at[index, column] = try_convert_to_float((row[column]))
+
+
+    for index, value in enumerate(df['timestamp']):
+        # if isinstance(value, float):
+        #     df.at[index, 'timestamp'] = pd.to_datetime(value, unit='s')
+        # else:
+        df.at[index, 'timestamp'] = convert_to_datetime(value)
+
+    return df
+
