a64dfbf4e90fe2b85a35dac2f0e6bfeed3205f04
cepurwin <cedric.purwin@outlook.de>
2024-03-11 10:46:16 +0100

    Velocity Berechnung ausgelagert

--- H5toDataframe.py
+++ H5toDataframe.py
@@ -24,17 +24,15 @@ def read_hdf5(file_path):
         max_length = max(len(values) for values in data_dict.values())
         shorter_columns = {name: len(values) for name, values in data_dict.items() if len(values) < max_length}
         if shorter_columns:
-            # data_dict['velocity'] = data_dict
-            # data_dict['velocity'] = [1, 1, 1, 1, 1]
             data_dict['velocity'] = np.empty(1000, dtype=float)
-            try:
-                for idx, distance in enumerate(data_dict['distance']):
-                    if idx == 0:
-                        data_dict['velocity'][idx] = 0
-                        continue
-                    data_dict['velocity'][idx] = data_dict['distance'][idx] / ((data_dict['timestamp'][idx] - data_dict['timestamp'][0])/1000)
-            except Exception as e:
-                print(e)
+            # try:
+            #     for idx, distance in enumerate(data_dict['distance']):
+            #         if idx == 0:
+            #             data_dict['velocity'][idx] = 0
+            #             continue
+            #         data_dict['velocity'][idx] = data_dict['distance'][idx] / ((data_dict['timestamp'][idx] - data_dict['timestamp'][0])/1000)
+            # except Exception as e:
+            #     print(e)
 
         dataframe = pd.DataFrame(data_dict)
 
